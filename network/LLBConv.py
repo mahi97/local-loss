d46c361a2d92d5a219906ea6c8ac5b5fae3b97fc
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2021-07-20 09:23:00 +0900

    remove code for loss and use loss classes

--- network/LLBConv.py
+++ network/LLBConv.py
@@ -3,8 +3,32 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-from network.utils import LinearFA, similarity_matrix
+from network.linear import LinearFA
+from network.loss import LossSim, LossPred, LossRecon
 import math
+
+
+def get_avg_pool_and_dim_in_decoder(dim_out, num_out, _dim_in_decoder):
+    # Resolve average-pooling kernel size in order for flattened dim to match self.args.dim_in_decoder
+    avg_pool = None
+    ks_h, ks_w = 1, 1
+    dim_out_h, dim_out_w = dim_out, dim_out
+    dim_in_decoder = num_out * dim_out_h * dim_out_w
+    while dim_in_decoder > _dim_in_decoder and ks_h < dim_out:
+        ks_h *= 2
+        dim_out_h = math.ceil(dim_out / ks_h)
+        dim_in_decoder = num_out * dim_out_h * dim_out_w
+        if dim_in_decoder > _dim_in_decoder:
+            ks_w *= 2
+            dim_out_w = math.ceil(dim_out / ks_w)
+            dim_in_decoder = num_out * dim_out_h * dim_out_w
+    if ks_h > 1 or ks_w > 1:
+        pad_h = (ks_h * (dim_out_h - dim_out // ks_h)) // 2
+        pad_w = (ks_w * (dim_out_w - dim_out // ks_w)) // 2
+        avg_pool = nn.AvgPool2d((ks_h, ks_w), padding=(pad_h, pad_w))
+
+    return avg_pool, dim_in_decoder
+
 
 class LocalLossBlockConv(nn.Module):
     '''
@@ -30,47 +54,23 @@ class LocalLossBlockConv(nn.Module):
                  dropout=None, bias=None, pre_act=False, post_act=True, args=None):
         super(LocalLossBlockConv, self).__init__()
         self.args = args
+        avg_pool, dim_in_decoder = get_avg_pool_and_dim_in_decoder(dim_out, ch_out, args.dim_in_decoder)
+        self.sim = LossSim(num_classes, ch_out, args, True, avg_pool)
+        self.pred = LossPred(num_classes, dim_in_decoder, args, avg_pool)
+        self.recon = LossRecon(ch_in, ch_out, args, first_layer, True, kernel_size, stride, padding, bias)
         self.ch_in = ch_in
         self.ch_out = ch_out
         self.num_classes = num_classes
         self.first_layer = first_layer
         self.dropout_p = self.args.dropout if dropout is None else dropout
-        self.bias = True if bias is None else bias
         self.pre_act = pre_act
         self.post_act = post_act
-        self.encoder = nn.Conv2d(ch_in, ch_out, kernel_size, stride=stride, padding=padding, bias=self.bias)
+        self.encoder = nn.Conv2d(ch_in, ch_out, kernel_size, stride=stride, padding=padding, bias=bias)
+        if args.nonlin == 'relu':
+            self.nonlin = nn.ReLU(inplace=True)
+        elif args.nonlin == 'leakyrelu':
+            self.nonlin = nn.LeakyReLU(negative_slope=0.01, inplace=True)
 
-        if not self.args.backprop and self.args.loss_unsup == 'recon':
-            self.decoder_x = nn.ConvTranspose2d(ch_out, ch_in, kernel_size, stride=stride, padding=padding)
-        if self.args.bio or (not self.args.backprop and (self.args.loss_sup == 'pred' or self.args.loss_sup == 'predsim')):
-            # Resolve average-pooling kernel size in order for flattened dim to match self.args.dim_in_decoder
-            ks_h, ks_w = 1, 1
-            dim_out_h, dim_out_w = dim_out, dim_out
-            dim_in_decoder = ch_out * dim_out_h * dim_out_w
-            while dim_in_decoder > self.args.dim_in_decoder and ks_h < dim_out:
-                ks_h *= 2
-                dim_out_h = math.ceil(dim_out / ks_h)
-                dim_in_decoder = ch_out * dim_out_h * dim_out_w
-                if dim_in_decoder > self.args.dim_in_decoder:
-                    ks_w *= 2
-                    dim_out_w = math.ceil(dim_out / ks_w)
-                    dim_in_decoder = ch_out * dim_out_h * dim_out_w
-            if ks_h > 1 or ks_w > 1:
-                pad_h = (ks_h * (dim_out_h - dim_out // ks_h)) // 2
-                pad_w = (ks_w * (dim_out_w - dim_out // ks_w)) // 2
-                self.avg_pool = nn.AvgPool2d((ks_h, ks_w), padding=(pad_h, pad_w))
-            else:
-                self.avg_pool = None
-        if not self.args.backprop and (self.args.loss_sup == 'pred' or self.args.loss_sup == 'predsim'):
-            if self.args.bio:
-                self.decoder_y = LinearFA(dim_in_decoder, self.args.target_proj_size, args)
-            else:
-                self.decoder_y = nn.Linear(dim_in_decoder, num_classes, args)
-            self.decoder_y.weight.data.zero_()
-        if not self.args.backprop and self.args.bio:
-            self.proj_y = nn.Linear(num_classes, self.args.target_proj_size, bias=False)
-        if not self.args.backprop and (self.args.loss_unsup == 'sim' or self.args.loss_sup == 'sim' or self.args.loss_sup == 'predsim'):
-            self.conv_loss = nn.Conv2d(ch_out, ch_out, 3, stride=1, padding=1, bias=False)
         if not self.args.no_batch_norm:
             if pre_act:
                 self.bn_pre = torch.nn.BatchNorm2d(ch_in)
@@ -78,14 +78,12 @@ class LocalLossBlockConv(nn.Module):
                 self.bn = torch.nn.BatchNorm2d(ch_out)
                 nn.init.constant_(self.bn.weight, 1)
                 nn.init.constant_(self.bn.bias, 0)
-        if self.args.nonlin == 'relu':
-            self.nonlin = nn.ReLU(inplace=True)
-        elif self.args.nonlin == 'leakyrelu':
-            self.nonlin = nn.LeakyReLU(negative_slope=0.01, inplace=True)
+
         if self.dropout_p > 0:
             self.dropout = torch.nn.Dropout2d(p=self.dropout_p, inplace=False)
         if self.args.optim == 'sgd':
-            self.optimizer = optim.SGD(self.parameters(), lr=0, weight_decay=self.args.weight_decay, momentum=self.args.momentum)
+            self.optimizer = optim.SGD(self.parameters(), lr=0, weight_decay=self.args.weight_decay,
+                                       momentum=self.args.momentum)
         elif self.args.optim == 'adam' or self.args.optim == 'amsgrad':
             self.optimizer = optim.Adam(self.parameters(), lr=0, weight_decay=self.args.weight_decay,
                                         amsgrad=self.args.optim == 'amsgrad')
@@ -160,74 +158,12 @@ class LocalLossBlockConv(nn.Module):
                 h = self.nonlin(h)
 
             # Calculate hidden feature similarity matrix
-            if self.args.loss_unsup == 'sim' or self.args.loss_sup == 'sim' or self.args.loss_sup == 'predsim':
-                if self.args.bio:
-                    h_loss = h
-                    if self.avg_pool is not None:
-                        h_loss = self.avg_pool(h_loss)
-                else:
-                    h_loss = self.conv_loss(h)
-                Rh = similarity_matrix(h_loss)
+            # loss_recon = self.recon(h)
+            loss_sim_u, loss_sim_s = self.sim(h, y_onehot)
+            # loss_pred = self.pred(x, y, y_onehot)
+            # loss_mahi = 0.0
 
-                # Calculate unsupervised loss
-            if self.args.loss_unsup == 'sim':
-                Rx = similarity_matrix(x).detach()
-                loss_unsup = F.mse_loss(Rh, Rx)
-            elif self.args.loss_unsup == 'recon' and not self.first_layer:
-                x_hat = self.nonlin(self.decoder_x(h))
-                loss_unsup = F.mse_loss(x_hat, x.detach())
-            else:
-                if self.args.cuda:
-                    loss_unsup = torch.cuda.FloatTensor([0])
-                else:
-                    loss_unsup = torch.FloatTensor([0])
-
-            # Calculate supervised loss
-            if self.args.loss_sup == 'sim':
-                if self.args.bio:
-                    Ry = similarity_matrix(self.proj_y(y_onehot)).detach()
-                else:
-                    Ry = similarity_matrix(y_onehot).detach()
-                loss_sup = F.mse_loss(Rh, Ry)
-                if not self.args.no_print_stats:
-                    self.loss_sim += loss_sup.item() * h.size(0)
-                    self.examples += h.size(0)
-            elif self.args.loss_sup == 'pred':
-                if self.avg_pool is not None:
-                    h = self.avg_pool(h)
-                y_hat_local = self.decoder_y(h.view(h.size(0), -1))
-                if self.args.bio:
-                    float_type = torch.cuda.FloatTensor if self.args.cuda else torch.FloatTensor
-                    y_onehot_pred = self.proj_y(y_onehot).gt(0).type(float_type).detach()
-                    loss_sup = F.binary_cross_entropy_with_logits(y_hat_local, y_onehot_pred)
-                else:
-                    loss_sup = F.cross_entropy(y_hat_local, y.detach())
-                if not self.args.no_print_stats:
-                    self.loss_pred += loss_sup.item() * h.size(0)
-                    self.correct += y_hat_local.max(1)[1].eq(y).cpu().sum()
-                    self.examples += h.size(0)
-            elif self.args.loss_sup == 'predsim':
-                if self.avg_pool is not None:
-                    h = self.avg_pool(h)
-                y_hat_local = self.decoder_y(h.view(h.size(0), -1))
-                if self.args.bio:
-                    Ry = similarity_matrix(self.proj_y(y_onehot)).detach()
-                    float_type = torch.cuda.FloatTensor if self.args.cuda else torch.FloatTensor
-                    y_onehot_pred = self.proj_y(y_onehot).gt(0).type(float_type).detach()
-                    loss_pred = (1 - self.args.beta) * F.binary_cross_entropy_with_logits(y_hat_local, y_onehot_pred)
-                else:
-                    Ry = similarity_matrix(y_onehot).detach()
-                    loss_pred = (1 - self.args.beta) * F.cross_entropy(y_hat_local, y.detach())
-                loss_sim = self.args.beta * F.mse_loss(Rh, Ry)
-                loss_sup = loss_pred + loss_sim
-                if not self.args.no_print_stats:
-                    self.loss_pred += loss_pred.item() * h.size(0)
-                    self.loss_sim += loss_sim.item() * h.size(0)
-                    self.correct += y_hat_local.max(1)[1].eq(y).cpu().sum()
-                    self.examples += h.size(0)
-
-            # Combine unsupervised and supervised loss
-            loss = self.args.alpha * loss_unsup + (1 - self.args.alpha) * loss_sup
+            loss = loss_sim_s #sum(map(lambda _x: _x[0] * _x[1], zip(self.args.abcde, [loss_recon, loss_sim_u, loss_sim_s, loss_pred])))
 
             # Single-step back-propagation
             if self.training:
